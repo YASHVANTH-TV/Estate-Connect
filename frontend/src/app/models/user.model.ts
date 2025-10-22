3aa13fae4f6ea8ada9a4530748f7b0609ebfea1b
YASHVANTH-TV <tvyashvanth12@gmail.com>
2025-10-22 11:04:06 +0530

    User & Login Models

---
+++ frontend/src/app/models/user.model.ts
@@ -0,0 +1,9 @@
+export interface User {
+    userId?: number;
+    email?: string;
+    password?: string;
+    firstName?: string;
+    lastName?: string;
+    mobileNumber?: string;
+    role?: string;
+}
