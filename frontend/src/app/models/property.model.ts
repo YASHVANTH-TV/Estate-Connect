b42b034b18957731a7113bba3c5126bbf62df180
YASHVANTH-TV <tvyashvanth12@gmail.com>
2025-10-22 12:18:08 +0530

    Property Model

---
+++ frontend/src/app/models/property.model.ts
@@ -0,0 +1,15 @@
+export interface Property {
+    propertyId?: number;
+    title: string;
+    description: string;
+    location: string;
+    price: number;
+    type: string;
+    status: string;
+    vaastu?: string;
+    riseType?: string;
+    floor?: number;
+    builder?: string;
+    techType?: string;
+    subType?: string;
+}
