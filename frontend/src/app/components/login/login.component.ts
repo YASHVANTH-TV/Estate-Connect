fa7b324cce3c1137773a51ac75538adf7c981da3
YASHVANTH-TV <tvyashvanth12@gmail.com>
2025-10-22 11:09:17 +0530

    Login & Signup Component

---
+++ frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private formBuidler: FormBuilder, private router: Router, private authService: AuthService) { }
+
+  loginForm: FormGroup;
+  loginFormData: any;
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuidler.group({
+      email: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    })
+  }
+
+  incorrectPassword: boolean = false;
+
+  login() {
+    if (this.loginForm.valid) {
+      this.loginFormData = this.loginForm.value;
+      this.authService.login(this.loginFormData).subscribe((result) => {
+        // localStorage.setItem('email', this.loginFormData.email);
+        this.router.navigate(['/home']);
+      }, (error) => {
+        // this.incorrectPassword = true;
+        console.log(error);
+      })
+    }
+  }
+}
